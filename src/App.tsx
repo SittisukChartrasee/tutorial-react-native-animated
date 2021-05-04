0d8c71af823c52dc2d79af3362b166078d652e3f
sittisuk <sittisuk@codefin.io>
2021-05-04 16:03:09 +0700

    add component flow animated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {View, SafeAreaView} from 'react-native';
-import {Todolist} from './pages';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import Navigator from './navigator'
+import NavigatorAnimated from './navigator/animated'
 
 export default () => {
   return (
     <Provider store={store}>
-      <Navigator />
+      {/* <Navigator /> */}
+      <NavigatorAnimated />
     </Provider>
   );
 };
